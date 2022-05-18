50214e7736f0ec3337ec38feb76deeab143cb510
ajsirh12 <ajsirh31@gmail.com>
2022-05-18 15:22:57 +0900

    STYLE: add annotation
    주석 추가

--- mongoDAO/src/mongo/model/MongoAuthModel.java
+++ mongoDAO/src/mongo/model/MongoAuthModel.java
@@ -1,22 +1,43 @@
 package mongo.model;
 
+/**
+ * MongoAuthModel <br>
+ * db, user, pwd
+ * @author LimDK
+ *
+ */
 public class MongoAuthModel {
 	private String USER;
 	private String DB;
 	private char[] PWD;
 	
+	/**
+	 * Set dbName, user, pwd
+	 * @param db
+	 * @param user
+	 * @param pwd
+	 */
 	public MongoAuthModel(String db, String user, String pwd) {
 		DB = db;
 		USER = user;
 		PWD = pwd.toCharArray();
 	}
 
+	/**
+	 * @return DB
+	 */
 	public String getDB() {
 		return DB;
 	}
+	/**
+	 * @return USER
+	 */
 	public String getUSER() {
 		return USER;
 	}
+	/**
+	 * @return PWD
+	 */
 	public char[] getPWD() {
 		return PWD;
 	}
